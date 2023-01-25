638a67150b4afb26b7cfb2a663acc32bde29441d
Logan Stapleton <lojostapleton@gmail.com>
2023-01-25 14:32:17 -0500

    Added Base CLI basic code

---
+++ base_cli/src/index.ts
@@ -0,0 +1,50 @@
+const { Command } = require("commander");
+const fs = require("fs");
+const path = require("path");
+const figlet = require("figlet");
+
+const program = new Command();
+
+console.log(figlet.textSync("Package Metrics"));
+
+program
+  .version("1.0.0")
+  .description("ACME package metric CLI")
+  .option("-u, --url  <value>", "inline url")
+  .option("-f, --file <value>", "absoulte directory of file containing urls")
+  .parse(process.argv);
+
+const options = program.opts();
+
+async function listDirContents(filepath: string) {
+  try {
+    const files = await fs.promises.readdir(filepath);
+    const detailedFilesPromises = files.map(async (file: string) => {
+      let fileDetails = await fs.promises.lstat(path.resolve(filepath, file));
+      const { size, birthtime } = fileDetails;
+      return { filename: file, "size(KB)": size, created_at: birthtime };
+    });
+    const detailedFiles = await Promise.all(detailedFilesPromises);
+    console.table(detailedFiles);
+  } catch (error) {
+    console.error("Error occurred while reading the directory!", error);
+  }
+}
+
+function createDir(filepath: string) {
+  if (!fs.existsSync(filepath)) {
+    fs.mkdirSync(filepath);
+    console.log("The directory has been created successfully");
+  }
+}
+
+function createFile(filepath: string) {
+  fs.openSync(filepath, "w");
+  console.log("An empty file has been created");
+}
+
+// if no options are passed show the help page
+if (!process.argv.slice(2).length) {
+    program.outputHelp();
+}
+  
