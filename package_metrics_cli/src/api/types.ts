9bd1ffccd20664947069fe4df7c34b97a3006499
Logan Stapleton <lojostapleton@gmail.com>
2023-01-30 23:32:02 -0500

    added license metric base code

--- package_metrics_cli/src/api/types.ts
+++ package_metrics_cli/src/api/types.ts
@@ -2,4 +2,6 @@ import { Endpoints } from "@octokit/types";
 
 export type communityProfileResponse = Endpoints["GET /repos/{owner}/{repo}/community/profile"]["response"];
 
+export type licenceResponse = Endpoints["GET /repos/{owner}/{repo}/license"]["response"];
+
 export type readmeResponse = Endpoints["GET /repos/{owner}/{repo}/readme"]["response"];
