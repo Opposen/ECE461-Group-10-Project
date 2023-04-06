9ea9fd6e7105fdf2d871f6cfd91188d065d47c24
rsego-hub <rsego@purdue.edu>
2023-04-06 15:07:45 -0400

    pulling in .env at start of tests

--- module_registry/.jest/setEnvVars.js
+++ module_registry/.jest/setEnvVars.js
@@ -1,2 +1,9 @@
-process.env.LOG_FILE = 'testlog'
-process.env.GITHUB_TOKEN = 'YOUR TOKEN HERE'
+var dotenv = require('dotenv');
+var path = require('path');
+
+console.log(`============ env-setup Loaded ===========`);
+dotenv.config({ path: path.resolve(process.cwd(), 'tests', 'settings', '/Users/robertsego/Documents/ECE461-Group-4-part-2/.env') });
+
+process.env.GITHUB_TOKEN;
+process.env.LOG_LEVEL;
+process.env.LOG_FILE;
