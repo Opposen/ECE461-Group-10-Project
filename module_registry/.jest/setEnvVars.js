8dd18b57277d0999f7dcb38ce146bd29cf1d224e
rsego-hub <rsego@purdue.edu>
2023-04-06 16:09:23 -0400

    removed local path, added automatic pathing to env

--- module_registry/.jest/setEnvVars.js
+++ module_registry/.jest/setEnvVars.js
@@ -1,8 +1,7 @@
 var dotenv = require('dotenv');
 var path = require('path');
 
-console.log(`============ env-setup Loaded ===========`);
-dotenv.config({ path: path.resolve(process.cwd(), 'tests', 'settings', '/Users/robertsego/Documents/ECE461-Group-4-part-2/.env') });
+dotenv.config({ path: path.resolve(process.cwd().substring(0, process.cwd().lastIndexOf('/')), '.env') });
 
 process.env.GITHUB_TOKEN;
 process.env.LOG_LEVEL;
