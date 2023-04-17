e06e06b09d2e92a9bc8d31a03aac6e09d89b5040
rsego-hub <rsego@purdue.edu>
2023-04-16 21:41:55 -0400

    reformatting for clarity, commenting, documenting, cleaning

--- webpage/loginScript.js
+++ webpage/loginScript.js
@@ -3,6 +3,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const User = exports.User;
 const UserDatabase = exports.UserDatabase;
 
+/**
+ * loads any file in without fs
+ * @param {*} filePath 
+ * @returns contents of file
+ */
 function loadFile(filePath) {
     var result = null;
     var xmlhttp = new XMLHttpRequest();
@@ -14,27 +19,22 @@ function loadFile(filePath) {
     return result;
 }
 
+/**
+ * gets contents of user storing file, converts to string
+ * @param {*} path to user file
+ * @returns each line of file as string
+ */
 function get_users(path) {
     const file_data = loadFile(path);
     const file_lines = file_data.split('\n');
     return file_lines;
 }
 
-
-const file_lines = get_users("user_list.txt")
-//logToFile(null, 2, "Read in users");
-
-
-// Create database from users read in, also add admin
-var database = new UserDatabase([]); // Create database, create admin but don't add
-const adminUser = new User('ece30861defaultadminuser','correcthorsebatterystaple123(!__+@**(A’”`;DROP TABLE packages;',true)
-database.addUser(adminUser);
-for(const line of file_lines) {
-    var user_vals = line.split(" ")
-    database.addUser(new User(user_vals[0], user_vals[1], false));
-}
-
-function can_login(event) {
+/**
+ * check if requirements met to login, give failure message to page if not met
+ * @param {*} event 
+ */
+function login_checks(event) {
     const errorElement = document.getElementById("error");
     const username = document.getElementById("username");
     const password = document.getElementById("password");
@@ -50,15 +50,9 @@ function can_login(event) {
     }
     else if (!database.can_login(username.value, password.value)) {
         messages.push("Incorrect username or password");
-        //logToFile(null, 2, "Successful login");
-        //event.preventDefault();
-        //window.location.href = "search/try.html";
-    }// else {
-        //logToFile(null, 2, "Failed login");
-        //alert('Incorrect Username or Password!');
-        
-    //}
+    }
 
+    // if there is a message, login did not succeed
     if (messages.length > 0) {
         event.preventDefault();
         errorElement.innerText = messages.join(",");
@@ -69,8 +63,20 @@ function can_login(event) {
     
 }
 
+// Read in file lines
+const file_lines = get_users("user_list.txt")
 
+// create database from users read in, also add admin
+var database = new UserDatabase([]); // Create database, create admin but don't add
+const adminUser = new User('ece30861defaultadminuser','correcthorsebatterystaple123(!__+@**(A’”`;DROP TABLE packages;',true)
+database.addUser(adminUser);
+for(const line of file_lines) {
+    var user_vals = line.split(" ")
+    database.addUser(new User(user_vals[0], user_vals[1], false));
+}
+
+// get page info, attatch login logic to login button
 const form = document.getElementById("form");
 if(form != null) {
-    form.addEventListener("submit", can_login);
+    form.addEventListener("submit", login_checks);
 }
